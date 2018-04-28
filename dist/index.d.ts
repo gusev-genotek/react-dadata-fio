9b409ea01bd43c3ad318f9d36a305f4df4f32c1f
Vladimir Gusev <gusev@genotek.ru>
2018-04-28 13:15:51 +0300

    first commit

---
+++ dist/index.d.ts
@@ -0,0 +1,53 @@
+/// <reference types="react" />
+import * as React from 'react';
+import './react-dadata.css';
+export declare namespace ReactDadataFio {
+    type DadataSuggestion = {
+        value: string;
+        unrestricted_value: string;
+        data: DadataFio;
+    };
+    type DadataFio = {
+      surname: string,
+      name: string,
+      patronymic: string,
+      gender: "MALE" | "FEMALE" | "UNKNOWN",
+    }
+    interface Props {
+        token: string;
+        placeholder?: string;
+        query?: string;
+        autoload?: boolean;
+        onChange?: (suggestion: DadataSuggestion) => void;
+    }
+    interface State {
+        query: string;
+        inputQuery: string;
+        inputFocused: boolean;
+        suggestions: Array<DadataSuggestion>;
+        suggestionIndex: number;
+        suggestionsVisible: boolean;
+    }
+}
+export declare class ReactDadataFio extends React.PureComponent<ReactDadata.Props, ReactDadata.State> {
+    /**
+     * HTML-input
+     */
+    protected textInput: HTMLInputElement;
+    /**
+     * XMLHttpRequest instance
+     */
+    protected xhr: XMLHttpRequest;
+    constructor(props: ReactDadata.Props);
+    componentDidMount(): void;
+    onInputFocus: () => void;
+    onInputBlur: () => void;
+    onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onKeyPress: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+    fetchSuggestions: () => void;
+    onSuggestionClick: (index: number, event: React.MouseEvent<HTMLDivElement>) => void;
+    selectSuggestion: (index: number) => void;
+    setCursorToEnd: (element: any) => void;
+    getHighlightWords: () => string[];
+    render(): JSX.Element;
+}
